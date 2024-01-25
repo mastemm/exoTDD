e9c0a3535634f2b0d5db954c43980c38df20761d
MassimaMao <mastem@msn.com>
2024-01-25 16:29:10 +0100

    RED - test search less than 2 characters

---
+++ RechercheVille.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice03
+{
+    public class RechercheVille
+    {
+        private List<string> _villes;
+
+        public List<string> Rechercher(string mot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RechercheVilleTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice03
+{
+    [TestClass]
+    public class RechercheVilleTest
+    {
+        private List<string> _villes;
+
+        private RechercheVille _rechercheVille;
+
+        
+
+        [TestMethod]
+        //1. Si le texte de la recherche contient moins de 2 caractères, ***Une exception est levée de type NotFoundException***.
+        public void WhenSearch_hasLessThan_2chars()
+        {
+            //Arrange
+            string mot = "A";
+            _rechercheVille = new RechercheVille();
+
+            //Act
+            _rechercheVille.Rechercher(mot);
+            //Assert
+
+            Assert.ThrowsException<Exception>(()=> _rechercheVille.Rechercher(mot));
+        }
+    }
+    
+}
